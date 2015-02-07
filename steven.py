9b2e482ff8b2711b5e940e1db7475f6910c716c1
unknown <kui.fu@KUI-FU-003.corp.vipshop.com>
2015-02-07 19:33:31 +0800

    modified:   steven.py 	modified:   res/effective_tld_names.dat

--- steven.py
+++ steven.py
@@ -9,13 +9,13 @@ import hashlib
 import urllib
 import urllib2
 
+
 class HTTPSQS():
     """
-    Low level HTTPSQS.
-    there should be a httpsqs server running first
+    low level httpsqs.
+    A httpsqs server should be running first
     httpsqs project : http://code.google.com/p/httpsqs/
     """
-
     @staticmethod
     def get_http_content(url_target, data_post=None,  timeout=30):
         opener = urllib2.build_opener()
@@ -37,6 +37,7 @@ class HTTPSQS():
             pass
         return r
 
+
     @staticmethod
     def get(queue_name, queue_host, queue_port, queue_charset, queue_password, queue_timeout):
         """
@@ -48,61 +49,51 @@ class HTTPSQS():
         :param queue_charset:charset for communicate
         :param queue_password:password for authentication
         :param queue_timeout:timeout for http request
-        :return:content got from queue server
+        :return:string content got from queue server
         """
         url_target = "http://%s:%s/?charset=%s&name=%s&opt=get&auth=%s" % (queue_host,
-                                                                            queue_port,
-                                                                            queue_charset,
-                                                                            queue_name,
-                                                                            queue_password)
-        queue_result = HTTPSQS.get_http_content(url_target, timeout=queue_timeout)
-        if queue_result == 'HTTPSQS_ERROR' or queue_result == '' or queue_result == 'HTTPSQS_GET_END':
-            return False
-        else:
-            return queue_result
+                                                                           queue_port,
+                                                                           queue_charset,
+                                                                           queue_name,
+                                                                           queue_password)
+        return HTTPSQS.get_http_content(url_target, timeout=queue_timeout)
+
 
 
     @staticmethod
-    def put(queue_name, queueData, queue_host, queue_port, queue_charset, queue_password, queue_timeout):
+    def put(queue_name, queue_data, queue_host, queue_port, queue_charset, queue_password, queue_timeout):
         """
         put one item to queue
         curl "http://host:port/?name=your_queue_name&opt=put&data=urlencodemsg&auth=mypass123
-        :param queue_name:
-        :param queueData:
-        :param queue_host:
-        :param queue_port:
-        :param queue_charset:
-        :param queue_password:
-        :param queue_timeout:
-        :return:
+        :param queue_name:string
+        :param queue_data:json json data for post
+        :param queue_host:string server name or ip address
+        :param queue_port:integer
+        :param queue_charset:string
+        :param queue_password:string
+        :param queue_timeout:integer
+        :return:string
         """
         url_target = "http://%s:%d/?charset=%s&name=%s&opt=put&auth=%s" % (queue_host,
-                                                                            queue_port,
-                                                                            queue_charset,
-                                                                            queue_name,
-                                                                            queue_password)
-        data_post = urllib.urlencode({'data':queueData})[5:]
-        queue_result = HTTPSQS.get_http_content(url_target, data_post, timeout=queue_timeout)
-        if queue_result == 'HTTPSQS_PUT_OK':
-            return True
-        elif queue_result == 'HTTPSQS_PUT_END':
-            return False
-        else:
-            return False
+                                                                           queue_port,
+                                                                           queue_charset,
+                                                                           queue_name,
+                                                                           queue_password)
+        data_post = urllib.urlencode({'data':queue_data})[5:]
+        return HTTPSQS.get_http_content(url_target, data_post, timeout=queue_timeout)
 
 
     @staticmethod
-    def status(queue_name, queue_host, queue_port, queue_charset, queue_password, queue_timeout):
+    def status(queue_name, queue_host, queue_port, queue_password, queue_timeout):
         """
         get status of queue
         curl "http://host:port/?name=your_queue_name&opt=status_json&auth=mypass123"
-        :param queue_name:
-        :param queue_host:
-        :param queue_port:
-        :param queue_charset:
-        :param queue_password:
-        :param queue_timeout:
-        :return:
+        :param queue_name:string
+        :param queue_host:string server name or ip address
+        :param queue_port:integer
+        :param queue_charset:string
+        :param queue_timeout:integer
+        :return:string
         """
         url_target = "http://%s:%d/?name=%s&opt=status_json&auth=%s" % (queue_host,
                                                                         queue_port,
@@ -112,34 +103,33 @@ class HTTPSQS():
 
 
     @staticmethod
-    def reset(queue_name, queue_host, queue_port, queue_charset, queue_password, queue_timeout):
+    def reset(queue_name, queue_host, queue_port, queue_password, queue_timeout):
         """
         reset a queue
         curl "http://host:port/?name=your_queue_name&opt=reset&auth=mypass123"
-        :param queue_name:
-        :param queue_host:
-        :param queue_port:
-        :param queue_charset:
-        :param queue_password:
-        :param queue_timeout:
-        :return:
+        :param queue_name:string
+        :param queue_host:string server name or ip address
+        :param queue_port:integer
+        :param queue_charset:string
+        :param queue_password:string
+        :param queue_timeout:integer
+        :return:string
         """
         url_target = "http://%s:%d/?name=%s&opt=reset&auth=%s" % (queue_host,
-                                                                 queue_port,
-                                                                 queue_name,
-                                                                 queue_password)
-        return HTTPSQS.get_http_content(url_target, timeout=queue_timeout) == 'HTTPSQS_RESET_OK'
+                                                                  queue_port,
+                                                                  queue_name,
+                                                                  queue_password)
+        return HTTPSQS.get_http_content(url_target, timeout=queue_timeout)
 
 
 class HQ():
-    """
-    High level HTTPSQS
-    """
+    """high level httpsqs"""
     queue_host     = '1.2.3.4'
     queue_port     = 1218
     queue_password = 'CHANGE_ME_TO_YOUR_PASSWORD'
     queue_charset  = 'utf-8'
     queue_timeout  = 30
+
 
     @staticmethod
     def put(queue_name, item):
@@ -150,7 +140,7 @@ class HQ():
         :return: True or False
         """
         return HTTPSQS.put(queue_name, item, HQ.queue_host, HQ.queue_port,
-                           HQ.queue_charset, HQ.queue_password, HQ.queue_timeout)
+                           HQ.queue_charset, HQ.queue_password, HQ.queue_timeout) == 'HTTPSQS_PUT_OK'
 
     @staticmethod
     def get(queue_name):
@@ -161,12 +151,13 @@ class HQ():
         """
         item = HTTPSQS.get(queue_name, HQ.queue_host, HQ.queue_port,
                            HQ.queue_charset, HQ.queue_password, HQ.queue_timeout)
-        if not item:
-            return ''
-        elif 'HTTPSQS_AUTH_FAILED' == item:
+        if 'HTTPSQS_GET_END' == item\
+                or 'HTTPSQS_AUTH_FAILED' == item\
+                or 'HTTPSQS_ERROR' == item:
             return ''
         else:
             return item
+
 
     @staticmethod
     def status(queue_name):
@@ -176,17 +167,17 @@ class HQ():
         :return:string
         """
         return HTTPSQS.status(queue_name, HQ.queue_host, HQ.queue_port,
-                              HQ.queue_charset, HQ.queue_password, HQ.queue_timeout).strip()
+                              HQ.queue_password, HQ.queue_timeout).strip()
 
     @staticmethod
     def reset(queue_name):
         """
         reset a queue
         :param queue_name:string
-        :return:string
+        :return:bool
         """
         return HTTPSQS.reset(queue_name, HQ.queue_host, HQ.queue_port,
-                             HQ.queue_charset, HQ.queue_password, HQ.queue_timeout)
+                             HQ.queue_password, HQ.queue_timeout) == 'HTTPSQS_RESET_OK'
 
     @staticmethod
     def unread(queue_name):
@@ -197,12 +188,12 @@ class HQ():
         """
         unread = 0
         status = HTTPSQS.status(queue_name, HQ.queue_host, HQ.queue_port,
-                                HQ.queue_charset, HQ.queue_password, HQ.queue_timeout)
+                                HQ.queue_password, HQ.queue_timeout)
         if status.find('unread') > -1:
             try:
                 unread = int(status.split('"unread":')[-1].split('}')[0])
-            except Exception, e:
-                unread = 999999999
+            except:
+                unread = -1
         return unread
 
 
@@ -212,15 +203,16 @@ class TLD():
     # Source path of Mozilla's effective TLD names file.
     http://mxr.mozilla.org/mozilla/source/netwerk/dns/src/effective_tld_names.dat?raw=1
     '''
-    TLD_DATA_FILE   = '.' + os.path.sep + 'res' + os.path.sep + 'effective_tld_names.dat'
-    TLD_NAME_LIST   = []
-    TLD_FILE_LOADED = False
+    __tld__data__file__   = '.' + os.path.sep + 'res' + os.path.sep + 'effective_tld_names.dat'
+    tld_names_list        = []
+    tld_data_loaded       = False
+
 
     @staticmethod
-    def initTLDNameList():
+    def init_tld_names_list():
         try:
-            fhandle            = open(TLD.TLD_DATA_FILE)
-            TLD.TLD_NAME_LIST  = set([line.strip() for line in fhandle if line[0] not in '/\n'])
+            fhandle            = open(TLD.__tld__data__file__)
+            TLD.tld_names_list = set([line.strip() for line in fhandle if line[0] not in '/\n'])
         except Exception, e:
             print e
         finally:
@@ -229,17 +221,17 @@ class TLD():
             except Exception, e2:
                 print e2
             finally:
-                if len(TLD.TLD_NAME_LIST) > 0:
-                    TLD.TLD_FILE_LOADED = True
+                if len(TLD.tld_names_list) > 0:
+                    TLD.tld_data_loaded = True
 
     @staticmethod
     def parse(domain, active_only=True):
         '''
-        input :mail.example.com.cn
+        input :test-04.videos.google.jp.co
         output:('example', 'com.cn', 'example.com.cn', 'mail.example.com.cn')
         '''
-        if TLD.TLD_FILE_LOADED  == False:
-            TLD.initTLDNameList()
+        if TLD.tld_data_loaded  == False:
+            TLD.init_tld_names_list()
 
         domain_parts = domain.split('.')
         for i in range(0, len(domain_parts)):
@@ -250,15 +242,21 @@ class TLD():
             inactive_match = "!%s" % match
 
             # Match tlds
-            if (match in TLD.TLD_NAME_LIST or
-                wildcard_match in TLD.TLD_NAME_LIST or
-                (active_only is False and inactive_match in TLD.TLD_NAME_LIST) ):
+            if (match in TLD.tld_names_list or
+                wildcard_match in TLD.tld_names_list or
+                (active_only is False and inactive_match in TLD.tld_names_list) ):
                 return (domain_parts[i-1], ".".join(domain_parts[i:]), ".".join(domain_parts[i-1:]), domain)
         return '', '', '', ''
 
 
     @staticmethod
     def get_tld_name(domain):
+        """
+        return the key word of domain, for example:
+        test-04.videos.google.com -> google
+        :param domain:string
+        :return:string
+        """
         return TLD.parse(domain)[0]
 
 
@@ -271,6 +269,7 @@ class C():
     NOTICE = 2
     INFO   = 1
     DEBUG  = 0
+
 
     priority = {7: 'EMERG', 6: 'ALERT', 5: 'CRIT', 4: 'ERROR',
                 3: 'WARN', 2: 'NOTICE', 1: 'INFO', 0: 'DEBUG'}
@@ -297,6 +296,10 @@ class C():
         return C.INFO
 
 class MD5():
+    """MD5 function string to hex"""
+    def __init__(self):
+        self.__init__()
+
     @staticmethod
     def md5(raw_message):
         """
@@ -328,11 +331,13 @@ if __name__ == '__main__':
     C.info('test')
     C.info('test', C.ERROR)
 
-    print TLD.get_tld_name('www.google.com.cn')
-    print TLD.parse('sina-xsdf.dd.google.com.cn')
+    domain = 'test-04.videos.google.co.jp'
+
+    print TLD.get_tld_name(domain)
+    print TLD.parse(domain)
 
     HQ.queue_host = 'example.httpsqs-server.com'
-    HQ.queue_password = 'mypassword'
+    HQ.queue_password = 'xxoo123'
     print HQ.put('test', 'test content')
     print HQ.status('test')
     print HQ.unread('test')
